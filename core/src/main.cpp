82e2f3b45e8c91e0c2a6370901f3e2ed7feaef50
Yu Zhang <1580074674@qq.com>
2018-06-15 08:45:28 +0200

    [Yu] Move converter as a library

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include "Converter.h"
 #include "TutorialConfig.h"
+#ifdef USE_CONVERTER
+#include "Converter.h"
+#endif
 
 int main() {
+#ifdef USE_CONVERTER
   Converter converter;
   printf("Hello From %s\n", converter.convert().c_str());
+#else
+  printf("Hello From %s\n", "Default Main");
+#endif
+
   printf("Version %d.%d\n", Tutorial_VERSION_MAJOR, Tutorial_VERSION_MINOR);
   return 0;
 }
